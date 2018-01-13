a43d82ccd265a5643d7a0d4e263951dc3832cc68
Stan <stanislav.ostrovskii10@my.stjohns.edu>
2018-01-13 03:04:34 -0500

    Updated layout.

--- app/src/main/java/com/stanislavveliky/macrotracker/MainActivity.java
+++ app/src/main/java/com/stanislavveliky/macrotracker/MainActivity.java
@@ -7,12 +7,6 @@ import android.support.v4.app.Fragment;
 public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-    }
-
-    @Override
     protected Fragment createFragment()
     {
         return new MainFragment();
--- app/src/main/java/com/stanislavveliky/macrotracker/MainFragment.java
+++ app/src/main/java/com/stanislavveliky/macrotracker/MainFragment.java
@@ -1,6 +1,10 @@
 package com.stanislavveliky.macrotracker;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by stan_ on 1/10/2018.
@@ -8,5 +12,19 @@ import android.support.v4.app.Fragment;
 
 public class MainFragment extends Fragment {
     private DailyTotal mDailyTotal;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mDailyTotal = DailyTotal.get(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        return v;
+    }
 
 }
